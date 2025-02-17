afcbd12a9a1df4cf753f2f3f990ee2e016922964
Yashu-Simform <yashu.ranparia@simformsolutions.com>
2025-02-17 09:27:31 +0530

    Update connection.py

--- NoSQL_Mongo/connection.py
+++ NoSQL_Mongo/connection.py
@@ -1,6 +1,6 @@
 class MongoInst:
     def __init__(self):
-        self.url = 'mongodb+srv://yashu:yashu@cluster0.3jtnp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+        self.url = 'mongodb+srv://yashu:*****@cluster0.3jtnp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
         pass
 
     def getUrl(self):
